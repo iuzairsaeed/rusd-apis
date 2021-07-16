e244929dc116e02ba0e1d159c50013346bf5380a
uzair saeed <uzair.saeed27@gmail.com>
2021-07-16 16:07:35 +0500

    add parent_id in Plan & set favico

--- database/migrations/2021_07_13_205136_create_plans_table.php
+++ database/migrations/2021_07_13_205136_create_plans_table.php
@@ -18,6 +18,7 @@ class CreatePlansTable extends Migration
             $table->string('name');
             $table->string('description');
             $table->string('type');
+            $table->foreignId('parent_id')->nullable()->default(0);
             $table->timestamps();
             $table->softDeletes();
         });
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
             <div class="col-12 d-flex align-items-center justify-content-center gradient-crystal-clear">
                 <div class="card px-4 py-2 box-shadow-2 width-400">
                     <div class="card-header text-center pb-0">
-                        <img src="/favicon.ico" alt="logo" class="main-logo mb-2 mt-3 width-300 height-100">
+                        <img src="favicon.ico" alt="logo" class="main-logo mb-2 mt-3 width-300 height-100">
                     </div>
                     <div class="card-body">
                         <div class="card-block">
